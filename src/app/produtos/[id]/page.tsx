562abe1c71362aa42af239984a147ccf33c40c49
unknown <gregorydealmeida901@gmail.com>
2024-11-26 15:19:30 -0300

    Add new loading

--- src/app/produtos/[id]/page.tsx
+++ src/app/produtos/[id]/page.tsx
@@ -3,6 +3,7 @@ import "@/app/produtos/[id]/produtos.css"
 import NavProdutos from "@/components/nav-produtos/nav-produtos";
 import {Usable, use, useEffect, useState } from "react";
 import Image, { StaticImageData } from "next/image";
+import Loading from "./loading";
 
 type PageParams = {
   params: Promise<{id: string }> & Usable<{id: string}>;
@@ -17,8 +18,8 @@ type IData = {
 }
 
 export default function ProdutoPage({params}: PageParams) {
-  //const API = 'https://e-commerce-gregory.netlify.app/'
-  const API = 'http://localhost:3000/'
+  const API = 'https://e-commerce-gregory.netlify.app/'
+  //const API = 'http://localhost:3000/'
   const [data, setData] = useState<IData[]>([])
   const {id}: {id: string} = use(params)
   
@@ -39,9 +40,14 @@ export default function ProdutoPage({params}: PageParams) {
       <NavProdutos />
       <h1 style={{textTransform: 'uppercase'}}>{id}</h1>
       <section>
-          {data.map(({id, nome, preco, img}) => (
+          {data.length < 1 ? <Loading /> : data.map(({id, nome, preco, img}) => (
           <div className="div-produto" key={id} >
-              <Image className="img-produto" src={img} height={200} alt="IMAGEM HARDWARE" />
+              <Image 
+                className="img-produto" 
+                src={img} 
+                height={200} 
+                alt="IMAGEM HARDWARE" 
+              />
               <p>{nome}</p>
               <h2>R$ {preco}</h2>
               <button>Comprar</button>
