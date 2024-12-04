8355f30069d82afe42722c2cb5214160d5245449
unknown <gregorydealmeida901@gmail.com>
2024-12-04 07:33:56 -0300

    fix

--- src/app/api/api.tsx
+++ src/app/api/api.tsx
@@ -8,8 +8,8 @@ export type IData = {
   img: StaticImageData;
 }
 
-export const API = 'http://localhost:3000/'
-//export const API = 'https://e-commerce-gregory.netlify.app/'
+//export const API = 'http://localhost:3000/'
+export const API = 'https://e-commerce-gregory.netlify.app/'
 
 export default async function GET_API(rota: string) {
   const response = await fetch(`${API}backend/${rota}`)
